5965c224c9814b77b51f553ae4e339a0eec3a338
Geoffrey Tisserand <geoffrey.tisserand@gmail.com>
2012-05-16 16:59:04 +0200

    modifications on actionbar

--- src/fr/utbm/lo52/sodia/ui/Settings.java
+++ src/fr/utbm/lo52/sodia/ui/Settings.java
@@ -2,9 +2,12 @@ package fr.utbm.lo52.sodia.ui;
 
 import fr.utbm.lo52.sodia.R;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Settings extends Activity {
 
@@ -22,6 +25,19 @@ public class Settings extends Activity {
 		Intent intent = new Intent(this, Main.class);
 	       intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 	       startActivity(intent);
+	}
+	
+	public void changeSettings(View view){
+		
+		Context context = getApplicationContext();
+		Toast toast = Toast.makeText(context, "Settings modified !", Toast.LENGTH_LONG);
+		toast.show();
+		
+		Intent intent = new Intent(this, Main.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(intent);
+		
+		
 	}
 	
 }
