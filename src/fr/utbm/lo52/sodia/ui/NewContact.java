7af94025c4149c51cb17f375e5aa9e78218d53fc
Geoffrey Tisserand <geoffrey.tisserand@gmail.com>
2012-05-16 16:43:37 +0200

    Remove hardcoded strings for action bar

--- src/fr/utbm/lo52/sodia/ui/NewContact.java
+++ src/fr/utbm/lo52/sodia/ui/NewContact.java
@@ -2,7 +2,11 @@ package fr.utbm.lo52.sodia.ui;
 
 import fr.utbm.lo52.sodia.R;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class NewContact extends Activity
@@ -17,5 +21,16 @@ public class NewContact extends Activity
 		super.onCreate(icicle);
 		setContentView(R.layout.newcontact);
 
+	}
+	
+	
+	public void addContact(View view){
+		Context context = getApplicationContext();
+		Toast toast = Toast.makeText(context, " Contact request sent !", Toast.LENGTH_LONG);
+		toast.show();
+		
+		Intent intent = new Intent(this, Main.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		startActivity(intent);
 	}
 }
