ce94af1a1270cfdb28c6db71095ae91111812afb
Antoine GRÉA <grea09@gmail.com>
2012-05-20 19:25:54 +0200

    Un ActionProvider qui fonctionne.

--- src/fr/utbm/lo52/sodia/ui/ChatsActionProvider.java
+++ src/fr/utbm/lo52/sodia/ui/ChatsActionProvider.java
@@ -1,15 +1,17 @@
 package fr.utbm.lo52.sodia.ui;
 
+
+
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.actionbarsherlock.view.ActionProvider;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
 import com.actionbarsherlock.view.SubMenu;
 
 import fr.utbm.lo52.sodia.R;
@@ -23,40 +25,45 @@ public class ChatsActionProvider extends ActionProvider implements OnMenuItemCli
 		mContext = context;
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Override
+	public boolean hasSubMenu() {
+		Log.d(this.getClass().getSimpleName(), "hasSubMenu");
+		return true;
+	}
 
 	@Override
 	public View onCreateActionView() {
 		 // Inflate the action view to be shown on the action bar.
-	    LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-	    View view = layoutInflater.inflate(R.layout.chats_action_provider, null);
-	    Button button = (Button) view.findViewById(R.id.chats);
-	    button.setOnClickListener(new View.OnClickListener() {
-	    	
-	        public void onClick(View v) {
-	       
-	        	Log.d(this.getClass().getSimpleName(), "onClick");
-
-	        }
-	    });
-	    return view;
+		LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+		View view = layoutInflater.inflate(R.layout.chats_action_provider, null);
+		Button button = (Button) view.findViewById(R.id.chats);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+			
+				Log.d(this.getClass().getSimpleName(), "onClick");
+			}
+		});
+		return view;
 	}
 	
+	@Override
 	public void onPrepareSubMenu(SubMenu subMenu) {
 		Log.d(this.getClass().getSimpleName(), "onPrepareSubMenu");
-		//subMenu.clear();
+		subMenu.clear();
 
 		for (int i = 0; i < 3; i++) {
-			subMenu.add("t");
+			subMenu.add(0, i, i, "Quel chat ?")
+					.setIcon(R.drawable.ic_launcher)
+					.setOnMenuItemClickListener(this);
 		}
 	}
-
-	public boolean hasSubMenu(){
-		return true;
-	}
 	
+	@Override
 	public boolean onMenuItemClick(MenuItem item) {
 		Toast.makeText(mContext, item.getTitle(), Toast.LENGTH_SHORT).show();
-		return false;
+		return true;
 	}
 
 }
