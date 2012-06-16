47199c6812e4aeb91c73363be6a0097c9d9670a5
Geoffrey Tisserand <geoffrey.tisserand@gmail.com>
2012-06-16 16:47:46 +0200

    use menu instead of actionprovider for attachedfile + intent gallery/audio + new way to append text

--- src/fr/utbm/lo52/sodia/ui/ChatActivity.java
+++ src/fr/utbm/lo52/sodia/ui/ChatActivity.java
@@ -7,8 +7,12 @@ import java.util.Set;
 import android.accounts.Account;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Html;
+import android.text.Layout;
 import android.text.method.ScrollingMovementMethod;
 import android.text.util.Linkify;
 import android.util.Log;
@@ -34,6 +38,10 @@ public class ChatActivity extends SherlockActivity implements ProtocolListener
 {
 
 	private Chat chat;
+	
+	private String selectedImagePath;
+    	private String selectedMusicPath;
+	
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -95,23 +103,21 @@ public class ChatActivity extends SherlockActivity implements ProtocolListener
 	}
 	
 	public void sendMessage(View view){
-		TextView t = (TextView)  findViewById(R.id.chatTextView);
 		EditText editText = (EditText) findViewById(R.id.chatEditText);
 		String smessage = editText.getText().toString();
 		
 		Message message = new Message(Mime.TEXT, smessage);
 		this.chat.add(message);
 		
+		//CLOSE KEYBOARD
 		editText.setText("");
 		if (this.getCurrentFocus() != null){
 			InputMethodManager inputManager = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE); 
 			inputManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 		}
-		t.append("\nMe @"+Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + ":" +
-				Calendar.getInstance().get(Calendar.MINUTE) + ":" +
-						Calendar.getInstance().get(Calendar.SECOND)+" > "+Html.fromHtml("")+":"+smessage);
 		
-		Linkify.addLinks(t, Linkify.ALL);
+		appendMessage(smessage);
+
 	}
 	
 	public boolean onOptionsItemSelected(MenuItem item) 
@@ -131,6 +137,23 @@ public class ChatActivity extends SherlockActivity implements ProtocolListener
 				Intent intent3 = new Intent(this, LocateActivity.class);
 				intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				startActivity(intent3);
+                            
+                                break;
+                            
+                        case (R.id.sendImage):
+				Intent intent4 = new Intent();
+				intent4.setType("image/*");
+				intent4.setAction(Intent.ACTION_GET_CONTENT);
+				startActivityForResult(Intent.createChooser(intent4, "Select Picture"), 1);
+                                break;
+                        case (R.id.sendMusic):
+				Intent intent5 = new Intent();
+				intent5.setType("audio/*");
+				intent5.setAction(Intent.ACTION_GET_CONTENT);
+				startActivityForResult(Intent.createChooser(intent5, "Select Music"), 2);
+				
+
+                                break;
 			default:
 				break;
 
@@ -153,4 +176,78 @@ public class ChatActivity extends SherlockActivity implements ProtocolListener
 		
 	}
 
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	    if (resultCode == RESULT_OK) {
+		switch (requestCode){
+		    case 1:
+			Uri selectedImageUri = intent.getData();
+
+			//OI FILE Manager
+			String filemanagerstring = selectedImageUri.getPath();
+
+			//MEDIA GALLERY
+			selectedImagePath = null;
+
+			String[] projection = { MediaStore.Images.Media.DATA };
+			Cursor cursor = managedQuery(selectedImageUri, projection, null, null, null);
+			if(cursor!=null)
+			{
+			    //HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+			    //THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+			    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+			    cursor.moveToFirst();
+			    selectedImagePath = cursor.getString(column_index);
+			}
+
+	
+			//DEBUG PURPOSE - you can delete this if you want
+			if(selectedImagePath!=null)
+			    System.out.println(selectedImagePath);
+			else System.out.println("selectedImagePath is null");
+			if(filemanagerstring!=null)
+			    System.out.println(filemanagerstring);
+			else System.out.println("filemanagerstring is null");
+
+			//NOW WE HAVE OUR WANTED STRING
+			if(selectedImagePath!=null)
+			    System.out.println("selectedImagePath is the right one for you!");
+			else
+			    System.out.println("filemanagerstring is the right one for you!");
+			
+			appendMessage("Your picture has been sent !");
+			break;
+			
+			
+		    case 2:
+			break;
+		    default:    
+			break;
+		}
+	    }else{
+		appendMessage("Nothing has been sent !");
+
+	    }
+	}
+	
+	
+	public void appendMessage(String m){
+		TextView t = (TextView)  findViewById(R.id.chatTextView);
+		Message message = new Message(Mime.PICTURE, "lol");
+
+		t.append("\nMe @"+Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + ":" +
+				Calendar.getInstance().get(Calendar.MINUTE) + ":" +
+						Calendar.getInstance().get(Calendar.SECOND)+" > "+m);
+		Linkify.addLinks(t, Linkify.ALL);
+		
+		//SCROLL DOWN THE TEXTVIEW
+		final Layout layout = t.getLayout();
+		if(layout != null){
+		    int scrollDelta = layout.getLineBottom(t.getLineCount() - 1) 
+			- t.getScrollY() - t.getHeight();
+		    if(scrollDelta > 0)
+			t.scrollBy(0, scrollDelta);
+		}
+	    
+	}
 }
